0d2a47300aa1fd987e85f888db8fee7120fa21c3
Theppasith N <tutorgaming@gmail.com>
2016-06-24 15:09:19 +0700

    Edit The file to support Mapping Paper

---
+++ build/custom_scan_matcher/CMakeFiles/run_tests_laser_scan_matcher.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/run_tests_laser_scan_matcher"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang)
+  include(CMakeFiles/run_tests_laser_scan_matcher.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/custom_scan_matcher/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/c3mx/robot/src/custom_scan_matcher
+# Build directory: /home/c3mx/robot/build/custom_scan_matcher
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(_ctest_laser_scan_matcher_rostest_test_run.test "/home/c3mx/robot/build/catkin_generated/env_cached.sh" "/usr/bin/python" "/opt/ros/jade/share/catkin/cmake/test/run_tests.py" "/home/c3mx/robot/build/test_results/laser_scan_matcher/rostest-test_run.xml" "--return-code" "/opt/ros/jade/share/rostest/cmake/../../../bin/rostest --pkgdir=/home/c3mx/robot/src/custom_scan_matcher --package=laser_scan_matcher --results-filename test_run.xml --results-base-dir \"/home/c3mx/robot/build/test_results\" /home/c3mx/robot/src/custom_scan_matcher/test/run.test ")
+add_test(_ctest_laser_scan_matcher_rostest_test_covariance.test "/home/c3mx/robot/build/catkin_generated/env_cached.sh" "/usr/bin/python" "/opt/ros/jade/share/catkin/cmake/test/run_tests.py" "/home/c3mx/robot/build/test_results/laser_scan_matcher/rostest-test_covariance.xml" "--return-code" "/opt/ros/jade/share/rostest/cmake/../../../bin/rostest --pkgdir=/home/c3mx/robot/src/custom_scan_matcher --package=laser_scan_matcher --results-filename test_covariance.xml --results-base-dir \"/home/c3mx/robot/build/test_results\" /home/c3mx/robot/src/custom_scan_matcher/test/covariance.test ")
